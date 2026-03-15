b644d8d3259749f063f5b66f54bcb65cc5318e69
Gea-Suan Lin <gslin@gslin.org>
2026-03-15 10:24:22 +0800

    fix: mariadb adapter.

--- src/db.js
+++ src/db.js
@@ -3,10 +3,15 @@ import { PrismaClient } from "./generated/prisma/client.ts";
 let prisma;
 
 if (process.env.NODE_ENV === "production") {
-  const datasourceUrl =
-    process.env.DATABASE_URL ||
-    `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}/${process.env.MYSQL_DATABASE}`;
-  prisma = new PrismaClient({ datasourceUrl });
+  const { PrismaMariaDb } = await import("@prisma/adapter-mariadb");
+  const adapter = new PrismaMariaDb({
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  });
+  prisma = new PrismaClient({ adapter });
 } else {
   const { PrismaBetterSqlite3 } = await import("@prisma/adapter-better-sqlite3");
   const url = process.env.DATABASE_URL || "file:./prisma/dev.db";
