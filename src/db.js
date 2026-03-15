50e2ede1f24f40ec0c9382d582c66772eff381f8
Gea-Suan Lin <gslin@gslin.org>
2026-03-15 10:14:28 +0800

    fix: still DATABASE_URL issue.

--- src/db.js
+++ src/db.js
@@ -3,7 +3,10 @@ import { PrismaClient } from "./generated/prisma/client.ts";
 let prisma;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  const datasourceUrl =
+    process.env.DATABASE_URL ||
+    `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}/${process.env.MYSQL_DATABASE}`;
+  prisma = new PrismaClient({ datasourceUrl });
 } else {
   const { PrismaBetterSqlite3 } = await import("@prisma/adapter-better-sqlite3");
   const url = process.env.DATABASE_URL || "file:./prisma/dev.db";
