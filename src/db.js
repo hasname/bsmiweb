8681f970ca792c77325617dd96b8fb5ce08b51c5
Gea-Suan Lin <gslin@gslin.org>
2026-03-15 10:10:43 +0800

    fix: use mysql on production.

--- src/db.js
+++ src/db.js
@@ -1,8 +1,14 @@
 import { PrismaClient } from "./generated/prisma/client.ts";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
-const url = process.env.DATABASE_URL || "file:./prisma/dev.db";
-const adapter = new PrismaBetterSqlite3({ url });
-const prisma = new PrismaClient({ adapter });
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  const { PrismaBetterSqlite3 } = await import("@prisma/adapter-better-sqlite3");
+  const url = process.env.DATABASE_URL || "file:./prisma/dev.db";
+  const adapter = new PrismaBetterSqlite3({ url });
+  prisma = new PrismaClient({ adapter });
+}
 
 export default prisma;
